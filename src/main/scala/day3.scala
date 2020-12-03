99a13ced72c61df1617b4c00a1aba841c6a067c2
John Ledbetter <john.ledbetter@gmail.com>
2020-12-03 18:04:11 -0500

    Day3: learn about iterators

--- src/main/scala/day3.scala
+++ src/main/scala/day3.scala
@@ -1,49 +1,32 @@
 package day3
 
-trait SlopeTraverser {
-  val velocity : (Int, Int)
+class SlopeIterator(val Velocity : (Int, Int), grid: Vector[String]) extends Iterator[Char] {
+  var pos = (0, 0)
 
-  def charAt(x: Int, y: Int) : Option[Char]
-
-  def countTrees: Int = {
-    var pos = (0, 0)
-    var ch : Option[Char] = None
-    var sum = 0
-
-    while ({
-      ch = charAt(pos._1, pos._2)
-      !ch.isEmpty
-    }) {
-      sum += (ch match {
-        case Some('#') => 1
-        case Some(_) => 0
-        case None => 0 // wont happen, is guarded in the loop above
-      })
-      pos = (pos._1 + velocity._1, pos._2 + velocity._2)
-    }
-
-    sum
+  /** True if the next Y position is in bounds of the list. */
+  def hasNext : Boolean = {
+     nextPos._2 < grid.size
   }
+
+  def next() : Char = {
+    pos = nextPos
+    charAt(pos)
+  }
+
+  private
+  def charAt(p: (Int, Int)) : Char = {
+    val (x, y) = p
+    val line = grid(y)
+    line.charAt(x % line.size)
+  }
+
+  private
+  def nextPos : (Int, Int) = (pos._1 + Velocity._1, pos._2 + Velocity._2)
 }
 
-class Day3(input: Iterator[String]) {
+class Day3(input: Iterator[String], val velocity: (Int, Int)) {
   val grid : Vector[String] = input.toVector
 
-  def charAt(x: Int, y: Int) : Option[Char] = {
-    if (y >= grid.size) {
-      None
-    } else {
-      val line = grid(y)
-      Some(line.charAt(x % line.size))
-    }
-  }
-}
-
-
-class Part1(input: Iterator[String]) extends Day3(input) with SlopeTraverser {
-  val velocity = (3, 1)
-}
-
-class Part2(input: Iterator[String], val velocity : (Int, Int)) extends Day3(input) with SlopeTraverser {
-
+  def toIterator : SlopeIterator = new SlopeIterator(velocity, grid)
+  def countTrees : Int = toIterator.count(_ == '#')
 }
--- src/test/scala/day3spec.scala
+++ src/test/scala/day3spec.scala
@@ -19,31 +19,16 @@ class Day3Spec extends AnyFlatSpec with Matchers {
     ".#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"
   )
 
-
-  "Day3" should "load the map correctly" in {
-    val items = List("#..", ".#.", "..#").toIterator
-    val day3 = new Day3(items)
-
-    day3.charAt(0, 0) shouldEqual Some('#')
-    day3.charAt(3, 0) shouldEqual Some('#')
-
-    day3.charAt(1, 1) shouldEqual Some('#')
-    day3.charAt(2, 2) shouldEqual Some('#')
-
-    day3.charAt(1, 3) shouldEqual None
-  }
-
   "Part1" should "calculate solution" in {
-    val p1 = new Part1(InputLines.forDay(3))
+    val p1 = new Day3(InputLines.forDay(3), (3, 1))
 
     p1.countTrees shouldEqual 223
-
   }
 
   "Part2" should "work for samples" in {
     val slopes = List((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
     val solution = slopes
-      .map(velocity => new Part2(sampleInput.toIterator, velocity))
+      .map(velocity => new Day3(sampleInput.toIterator, velocity))
       .map(_.countTrees)
       .reduce((a, b) => a * b)
 
@@ -54,7 +39,7 @@ class Day3Spec extends AnyFlatSpec with Matchers {
     val slopes = List((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
 
     val solution = slopes
-      .map(velocity => new Part2(InputLines.forDay(3), velocity))
+      .map(velocity => new Day3(InputLines.forDay(3), velocity))
       .map(_.countTrees.toLong)
       .reduce((a, b) => a * b)
 
