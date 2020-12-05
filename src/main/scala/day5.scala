8da727a98402945a1f39b4bf077329fe0bffd0f5
John Ledbetter <john.ledbetter@gmail.com>
2020-12-05 11:48:24 -0500

    Cleanup Day5

--- src/main/scala/day5.scala
+++ src/main/scala/day5.scala
@@ -1,31 +1,14 @@
 package day5
 
-class BoardingPass(encoding: String) {
-  val Rows = 128
-  val Cols = 8
-
-  val row = Integer.parseInt(encoding
-    .substring(0, 7)
-    .map(ch => ch match {
-      case 'B' => 1
-      case _ => 0
-    })
-    .map(_.toString)
-    .mkString, 2)
-
-  val col = Integer.parseInt(encoding
-    .substring(7)
-    .map(ch => ch match {
-      case 'R' => 1
-      case _ => 0
-    })
-    .map(_.toString)
-    .mkString, 2)
-
-  val seatId = row * 8 + col
-}
-
 class Day5(input: Iterator[String]) {
-  val passes = input.map(new BoardingPass(_)).toList
+  val seats = input.map(codeToSeatId).toSet
 
+  private def codeToSeatId(encoded: String) : Int = encoded
+    .map { case 'B' | 'R' => 1 ; case _ => 0 }
+    .foldLeft(0) { case (result, bit) => result << 1 | bit }
+
+  // Part 1
+  def maxSeatId : Int = seats.max
+  // Part2
+  def mySeatId : Option[Int] = (0 until 1024).find { id => !seats(id) && seats(id + 1) && seats(id - 1) }
 }
